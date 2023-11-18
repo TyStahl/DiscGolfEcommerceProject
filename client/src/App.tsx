2ef210003559da8f62616a294516189b8be28a36
Tyler Stahl <tstahl488@gmail.com>
2023-11-18 00:28:52 +0000

    basic functional css, layout for product cards, filter menu, catalog spacing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Header } from './Header';
-import { DiscCard, DiscArray } from './DiscCard';
+import { DiscCatalog } from './pages/DiscCatalog';
+import { Header } from './components/Header';
 
 export default function App() {
-  const [discData, setDiscData] = useState<DiscArray>([]);
-
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/discs');
-      const data: DiscArray = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setDiscData(data);
-    }
-
-    readServerData();
-  }, []);
-
   return (
     <>
       <Header />
-      <DiscCard discData={discData} />
+      <DiscCatalog />
     </>
   );
 }
